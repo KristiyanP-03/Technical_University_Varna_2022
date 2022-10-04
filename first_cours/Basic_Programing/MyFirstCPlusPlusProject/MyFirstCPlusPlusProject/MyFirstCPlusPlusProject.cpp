481d02bff0c66d9dec7cea3d43bc0db848209cb4
KristiyanP-03 <114045349+KristiyanP-03@users.noreply.github.com>
2022-10-03 20:07:42 -0700

    C++ project uploaded LOL

---
+++ first_cours/Basic_Programing/MyFirstCPlusPlusProject/MyFirstCPlusPlusProject/MyFirstCPlusPlusProject.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	// zad 1
+	/* 
+	cout << "Hello world" << endl;
+	*/
+
+	// zad 2
+	/* 
+	char name[20], Sp[5];
+	long FN;
+	char ans;
+	int course, group;
+	cout << "\n What's your name:";
+	cin.getline(name, 20);
+	cout << "\n Wallcome to TU - Varna " << name << endl;
+	cout << "\n What's your major:";
+	cin >> Sp;
+	cout << Sp << " is the best choice for you\n";
+	cout << "\n Faculty number:";
+	cin >> FN;
+	cout << "\n Don't forget your it: " << FN << " is important feature of every student" << endl;
+	cout << "\n Which year student are you:";
+	cin >> course;
+	cout << "\ you are absolute beginner - first year student" << endl;
+	cout << "\n Number of group:";
+	cin >> group;
+	cout << group << " is the funniest group - enjoy the class" << endl;
+	cout << "\n Are you a girl [y/n]:";
+	cin >> ans;
+	if (ans == 'y' || ans == 'Y')
+		cout << "female" << endl;
+	else
+		cout << "male" << endl;
+	*/
+
+	//zad 3
+	/*
+	float a, b, c, d, f;
+	cout << "\n a=";
+	cin >> a;
+	cout << "\n b=";
+	cin >> b;
+	cout << "\n c=";
+	cin >> c;
+	d = (a + b) / a - c / (a - b);
+	cout << "\n Result d=" << d << endl;
+	f = (3 + 4 * a) / (4 - 2 * c) + ((8 - a * a) * (4 + 2 * c)) / (b - c);
+	cout << "\n f=" << f << endl;
+	*/
+}
