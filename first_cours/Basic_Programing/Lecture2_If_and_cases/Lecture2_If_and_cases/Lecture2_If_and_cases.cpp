41a38572503c6aabe38eea9b025f8dbbdceebbc3
KristiyanP-03 <114045349+KristiyanP-03@users.noreply.github.com>
2022-10-12 21:03:42 -0700

    Some work

---
+++ first_cours/Basic_Programing/Lecture2_If_and_cases/Lecture2_If_and_cases/Lecture2_If_and_cases.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //zad. 1 - Simple string and int cout conversation
+    /* 
+    string name;
+    int age;
+
+    cout << "Enter your name: " << endl;
+    cin >> name;
+    cout << "Enter your age: " << endl;
+    cin >> age;
+
+    cout << "\nHello, " << name 
+        << ". You are " << age << " y/o, right?" << endl;
+    
+    return 0;
+    */
+
+    // zad. 2 - Something similar buth with C's codes
+    /*
+    char name[20];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s" , &name);
+    printf("Enter your age: ");
+    scanf("%d" , &age);
+
+    printf("You are %s", name);
+    printf(". You are %d", age);
+    printf(" y/o, right?");
+
+    return 0;
+    */
+
+
+}
