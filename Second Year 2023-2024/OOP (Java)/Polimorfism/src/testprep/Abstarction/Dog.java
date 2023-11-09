ac4d1baffb1c3433b68fe927ade90f716ffaf6cc
Kristiyan Pisev <kristiyanpisev@gmail.com>
2023-11-09 09:39:26 +0200

    Test prep

---
+++ Second Year 2023-2024/OOP (Java)/Polimorfism/src/testprep/Abstarction/Dog.java
@@ -0,0 +1,9 @@
+package testprep.Abstarction;
+
+//Наследник наследяващ класа Annimal
+public class Dog extends Animal {
+
+    public void sound() {
+        System.out.println("Woof");
+    }
+}
---
+++ Second Year 2023-2024/OOP (Java)/Polimorfism/src/testprep/Abstarction/А.java
@@ -0,0 +1,12 @@
+package testprep.Abstarction;
+
+//Деклариране на клас с ключовата дума abstract
+abstract class A{
+    //Абстрактен метод без тяло
+    abstract void myMethod();
+
+    //Конкретен метод с тяло
+    void anotherMethod(){
+        //реализация
+    }
+}
