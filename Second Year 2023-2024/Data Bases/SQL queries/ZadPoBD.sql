ad34d05ae149f9089e4da17be5f467403151a77e
Kristiyan Pisev <kristiyanpisev@gmail.com>
2023-11-29 20:47:33 +0200

    Database Entries created

--- Second Year 2023-2024/Data Bases/SQL queries/ZadPoBD.sql
+++ Second Year 2023-2024/Data Bases/SQL queries/ZadPoBD.sql
@@ -69,3 +69,134 @@ CREATE TABLE Position_Employee (
     pos_id INT PRIMARY KEY,
     position_name VARCHAR2(20)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Car_Brand Table
+INSERT INTO Car_Brand (brand_id, brand_title, brand_model_id) VALUES
+(1, 'Toyota', 1);
+
+INSERT INTO Car_Brand (brand_id, brand_title, brand_model_id) VALUES
+(2, 'Honda', 2);
+
+INSERT INTO Car_Brand (brand_id, brand_title, brand_model_id) VALUES
+(3, 'Ford', 3);
+
+
+
+-- Brand_Model Table
+INSERT INTO Brand_Model (model_id, model_title) VALUES
+(1, 'Camry');
+
+INSERT INTO Brand_Model (model_id, model_title) VALUES
+(2, 'Civic');
+
+INSERT INTO Brand_Model (model_id, model_title) VALUES
+(3, 'Focus');
+
+
+
+
+
+-- Car_Color Table
+INSERT INTO Car_Color (color_id, color_name) VALUES
+(1, 'Red');
+
+INSERT INTO Car_Color (color_id, color_name) VALUES
+(2, 'Blue');
+
+INSERT INTO Car_Color (color_id, color_name) VALUES
+(3, 'Silver');
+
+
+
+
+
+
+-- Car Table
+INSERT INTO Car (car_id, car_brand_id, car_color_id, year, mileage, price) VALUES
+(1, 1, 1, 2020, 30000, 25000.00);
+
+INSERT INTO Car (car_id, car_brand_id, car_color_id, year, mileage, price) VALUES
+(2, 2, 2, 2019, 28000, 22000.50);
+
+INSERT INTO Car (car_id, car_brand_id, car_color_id, year, mileage, price) VALUES
+(3, 3, 3, 2021, 25000, 20000.75);
+
+
+
+
+
+
+-- Position_Employee Table
+INSERT INTO Position_Employee (pos_id, position_name) VALUES
+(1, 'Sales Representative');
+
+INSERT INTO Position_Employee (pos_id, position_name) VALUES
+(2, 'Manager');
+
+INSERT INTO Position_Employee (pos_id, position_name) VALUES
+(3, 'Technician');
+
+
+
+
+
+
+-- Employee Table
+INSERT INTO Employee (employee_id, employee_pos_id, name, phone_number) VALUES
+(1, 1, 'John Doe', '1234567890');
+
+INSERT INTO Employee (employee_id, employee_pos_id, name, phone_number) VALUES
+(2, 2, 'Jane Smith', '9876543210');
+
+INSERT INTO Employee (employee_id, employee_pos_id, name, phone_number) VALUES
+(3, 3, 'Bob Johnson', '5678901234');
+
+
+
+
+-- Client Table
+INSERT INTO Client (client_id, name, address, phone_number) VALUES
+(1, 'Alice Williams', '123 Main St, Cityville', '5551112233');
+
+INSERT INTO Client (client_id, name, address, phone_number) VALUES
+(2, 'Charlie Brown', '456 Oak St, Townsville', '5552223344');
+
+INSERT INTO Client (client_id, name, address, phone_number) VALUES
+(3, 'Eva Davis', '789 Pine St, Villagetown', '5553334455');
+
+
+
+
+
+
+
+-- Sales Table
+INSERT INTO Sales (purchase_id, client_buyer_id, car_buyer_id, employee_seller_id, date_of_sale) VALUES
+(1, 1, 1, 1, '2023-01-15');
+
+INSERT INTO Sales (purchase_id, client_buyer_id, car_buyer_id, employee_seller_id, date_of_sale) VALUES
+(2, 2, 2, 2, '2023-02-20');
+
+INSERT INTO Sales (purchase_id, client_buyer_id, car_buyer_id, employee_seller_id, date_of_sale) VALUES
+(3, 3, 3, 3, '2023-03-25');
