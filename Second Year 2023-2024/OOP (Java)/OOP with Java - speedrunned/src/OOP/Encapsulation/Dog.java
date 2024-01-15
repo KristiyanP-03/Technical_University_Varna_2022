84a26121c1b164021fd1b94d00069644be8aeca3
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2024-01-15 17:34:26 +0200

    Java OOP speedrunned - Encapsulation

---
+++ Second Year 2023-2024/OOP (Java)/OOP with Java - speedrunned/src/OOP/Encapsulation/Dog.java
@@ -0,0 +1,10 @@
+package OOP.Encapsulation;
+
+public class Dog extends Animal{
+    private String breed;
+
+    public Dog(String name, int age, String breed){
+        super(name, age);
+        this.breed = breed;
+    }
+}
