a6ccce8709a768acafdcdc4da8b6de364c3b0cbe
Kristiyan Pisev <kristiyanpisev@gmail.com>
2024-03-09 23:15:28 +0200

    Creating Windows Form Apps with C/C++

---
+++ Second Year 2023-2024/Programing Systems/Lab3/project/project/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by project.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PROJECT_DIALOG              102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PROJECT                     107
+#define IDI_SMALL                       108
+#define IDC_PROJECT                     109
+#define IDR_MAINFRAME                   128
+#define IDD_DLG_MYFIRST                 129
+#define IDR_MENU1                       130
+#define IDR_ACCELERATOR1                131
+#define IDC_RADIO1                      1000
+#define IDC_RADIO2                      1001
+#define IDC_RADIO3                      1002
+#define IDC_RADIO4                      1003
+#define IDC_BUTTON1                     1004
+#define IDC_CHECK1                      1005
+#define IDC_CHECK2                      1006
+#define IDC_EDIT1                       1007
+#define ID_FILE_MYFIRST                 32771
+#define ID_FILE_NESH                    32772
+#define ID_FILE_MYFIRSTDIALOG           32773
+#define ID_FILE_MYFIRSTDIAL             32775
+#define ID_FILE_NESH32776               32776
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
